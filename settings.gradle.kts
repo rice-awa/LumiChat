a4aea5aad1949cd760de4ba39b142515a5baacc0
rice-awa <141110807+rice-awa@users.noreply.github.com>
2026-02-21 09:45:20 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.kikugie.dev/snapshots") { name = "KikuGie Snapshots" }
+    }
+}
+
+plugins {
+    id("dev.kikugie.stonecutter") version "0.8.3"
+}
+
+stonecutter {
+    create(rootProject) {
+        // See https://stonecutter.kikugie.dev/wiki/start/#choosing-minecraft-versions
+        versions("1.20.1", "1.21.1", "1.21.10", "1.21.11")
+        vcsVersion = "1.21.11"
+    }
+}
+
+rootProject.name = "Template"
