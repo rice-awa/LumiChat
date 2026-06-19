d6586b81fb20db75d39e0eb4c75103bd46a8ef6f
rice-awa <141110807+rice-awa@users.noreply.github.com>
2026-06-19 08:00:46 +0000

    refactor(mappings): Mixin 目标/访问器字段迁移至 Mojang 命名

--- src/main/java/com/riceawa/mixin/ServerPlayerEntityAccessor.java
+++ src/main/java/com/riceawa/mixin/ServerPlayerEntityAccessor.java
@@ -1,44 +1,21 @@
 package com.riceawa.mixin;
 
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.network.ServerPlayerInteractionManager;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.World;
+import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 /**
- * Accessor for ServerPlayerEntity and related classes.
+ * Accessor for ServerPlayer and related classes.
  * This is needed because Minecraft 1.21.11 removed some public methods.
  */
-@Mixin(ServerPlayerEntity.class)
+@Mixin(ServerPlayer.class)
 public interface ServerPlayerEntityAccessor {
-    
+
     /**
-     * Access the private server field in ServerPlayerEntity.
+     * Access the private server field in ServerPlayer.
      * @return The MinecraftServer instance
      */
     @Accessor("server")
     MinecraftServer getServerInstance();
-}
-
-/**
- * Accessor for ServerPlayerInteractionManager to get the world.
- */
-@Mixin(ServerPlayerInteractionManager.class)
-interface ServerPlayerInteractionManagerAccessor {
-    
-    /**
-     * Access the protected world field.
-     * @return The ServerWorld instance
-     */
-    @Accessor("world")
-    ServerWorld getWorld();
-    
-    /**
-     * Set the world field.
-     */
-    @Accessor("world")
-    void setWorld(ServerWorld world);
 }
