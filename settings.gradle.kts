85f7ffe2caa028104dd508b007d46b063b2897e0
rice-awa <q77884590@163.com>
2026-07-22 00:11:47 +0800

    ci: 添加opencode工作流

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,7 @@
 pluginManagement {
     repositories {
+        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
+        maven("https://mirrors.cloud.tencent.com/nexus/repository/gradle-plugin/")
         mavenLocal()
         mavenCentral()
         gradlePluginPortal()
